034599d042274aac0a5157330eeba21a135cf663
ajitkumar2004 <kumarmallikajit70@gmail.com>
2025-11-14 21:34:12 +0530

    Update SecurityConfig.java

--- src/main/java/io/reflectoring/DigitaLibraries/SecurityConfig.java
+++ src/main/java/io/reflectoring/DigitaLibraries/SecurityConfig.java
@@ -1,40 +1,62 @@
 package io.reflectoring.DigitaLibraries.Configuration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-{
-    http.authorizeHttpRequests(req->req.anyRequest().authenticated())
-            .formLogin(form->form.permitAll())
-            .logout(LogoutConfigurer::permitAll);
-return http.build();
-}
-@Bean
-public PasswordEncoder passwordEncoder()
-{
-    return new BCryptPasswordEncoder();
-}
-@Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder)
-{
-    UserDetails userDetails = User.withUsername("friendUser")
-            .password(passwordEncoder.encode("12345"))
-            .roles("FRIEND")
-            .build();
-    return new InMemoryUserDetailsManager(userDetails);
-}
 
+    private final UserService userService;
 
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Will explain later or in next class
+                // CSRF = Cross-Site Request Forgery
+                .csrf(AbstractHttpConfigurer::disable)
+                // Which API calls require authentication are written here:
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/redis/**").permitAll()
+                                .anyRequest()
+                                .authenticated())
+                // Then we write the method of logging in
+                // 1. FormLogin -> The default login page that spring gives
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // 1234
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(this.passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
